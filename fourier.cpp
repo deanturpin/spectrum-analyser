57eb5d1d76d2b26e9bb1938f7eb3ac174f452f12
deanturpin <git@lvbtn.uk>
2017-08-28 15:14:37 +0100

    Implement ASCII keyboard - closes #41
    - Process all bins again
    - Add list of keys
    - Remove commented code
    - Remove unused headers
    - Remove original ASCII histogram

--- fourier.cpp
+++ fourier.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <complex>
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <vector>
 
 void fourier();
@@ -19,7 +17,7 @@ void fourier() try {
 
   // The number of bins is fundamental. It's the number of samples to read,
   // size of the twiddle matrix and the resulting Fourier transform.
-  const unsigned int bins = 1000;
+  const unsigned int bins = 2000;
 
   // Read a batch of samples
   vector<short> samples(bins);
@@ -34,16 +32,6 @@ void fourier() try {
       twiddle[n][k] =
           exp(complex<double>(0, 2) * M_PI * static_cast<double>(k) *
               static_cast<double>(n) / static_cast<double>(bins));
-
-  // Windowing
-#if 0
-  const unsigned int window_size = 100;
-  for (unsigned int i = 0; i < window_size; ++i)
-    samples.at(i) *= (i + 1) / window_size;
-
-  for (unsigned int i = 0; i < window_size; ++i)
-    samples.at(samples.size() - 1 - i) *= (i + 1) / window_size;
-#endif
 
   // The Fourier transform is the dot product of the twiddle matrix and the
   // original samples. Only run over the first half of the matrix as the other
@@ -65,135 +53,55 @@ void fourier() try {
   // Bin resolution
   const double bin_resolution = wav.sample_rate / static_cast<double>(bins);
 
+  cout << "CHORD LORD" << endl;
   cout << "Sample rate " << wav.sample_rate << " Hz" << endl;
   cout << "Bin resolution " << bin_resolution << " Hz" << endl;
 
-  // Print some keys
-  /*
-  unsigned int keys = 0;
-  for (const auto &key : riff::notes) {
-    cout << "| " << key.second << "\t|" << endl;
-
-    if (++keys > 12)
-      break;
-  }
-  */
-
-  const auto red = "\033[41m";
-  const auto white = "\033[0m";
-  const auto yellow = "\033[33m";
-
-  // cout << yellow << "|    |" << endl;
-  // cout << red << "| xxx|" << endl;
-  // cout << yellow << "|    |" << endl;
-  // cout << red << "| xxx|" << endl;
-  // cout << yellow << "|    |" << endl;
-  // cout << yellow << "|    |" << endl;
-  // cout << red << "| xxx|" << endl;
-  // cout << yellow << "|    |" << endl;
-  // cout << red << "| xxx|" << endl;
-  // cout << yellow << "|    |" << endl;
-  // cout << red << "| xxx|" << endl;
-  // cout << yellow << "|    |" << white << endl;
-
-  /*
-  vector<string> scale = {
-    "C ",
-    "C#",
-    "D ",
-    "D#",
-    "E ",
-    "F ",
-    "F#",
-    "G ",
-    "G#",
-    "A ",
-    "A#",
-    "B",
-  };
-
-  for (unsigned int i = 0; i < 24; ++i)
-    cout << scale.at(i % 12) << " " << dec;
-
-  cout << "|" << endl;
-  */
-
+  // ASCII representation of an octave on the piano
   vector<string> keys = {
-    " ",
-    "|",
-    " ",
-    "|",
-    " ",
-    " ",
-    "|",
-    " ",
-    "|",
-    " ",
-    "|",
-    " ",
+      " ", " ", "|", " ", "|", " ", " ", "|", " ", "|", " ", "|",
   };
 
+  const unsigned int key_count = 72;
+  const unsigned int notes_in_an_octave = 12;
 
   // Construct ASCII keyboard
-  const unsigned int key_count = 72;
   string keyboard;
   for (unsigned int i = 0; i < key_count; ++i)
-    keyboard += keys.at(i % 12);
+    keyboard += keys.at(i % notes_in_an_octave);
 
-  keyboard += "|";
   cout << string(key_count, '_') << endl;
   cout << keyboard << endl;
-  cout << keyboard << " Chord Lord" << endl;
+  cout << keyboard << endl;
 
-  // Find the max element so we know how much to scale the results
-  // const auto max_bin = *max_element(fourier.cbegin(), fourier.cend());
-  const auto max_bin = numeric_limits<short>::max() / 20;
+  // Find the max element so we know how large the peaks will be
+  const auto max_bin = *max_element(fourier.cbegin(), fourier.cend());
 
-  // Print the Fourier transform as an ASCII art histogram. Each bin is
-  // converted into a bar.
+  // Pass over the results and calculate corresponding piano key
   string key_strikes = string(key_count, ' ');
-  for (unsigned int bin = 50; bin < fourier.size(); ++bin) {
+  map<string, bool> key_list;
+  for (unsigned int bin = 0; bin < fourier.size(); ++bin) {
 
     const double current_bin = fourier.at(bin);
     const double bin_freq = bin * bin_resolution;
 
     if (current_bin > max_bin / 2) {
+
       const auto note = riff::notes.lower_bound(bin_freq);
       const auto key = distance(riff::notes.cbegin(), note) % key_count;
-      key_strikes.at(key - 1) = '^';
+
+      key_strikes.at(key) = 'O';
+      key_list[note->second] = true;
     }
   }
 
-  cout << key_strikes << endl;
   cout << string(key_count, '_') << endl;
+  cout << key_strikes << endl;
 
-  for (unsigned int bin = 50; bin < fourier.size(); ++bin) {
-
-    const double bin_freq = bin * bin_resolution;
-
-    // Normalise the results and scale to make the graph fit nicely into the
-    // terminal. The absolute value of the (complex) Fourier result is used to
-    // calculate the bar length.
-    const double full_bar = 75.0;
-    const double current_bin = fourier.at(bin);
-    const auto bar_length =
-        static_cast<unsigned int>(round(full_bar * current_bin / max_bin));
-
-    // Print the bar and make it colourful
-    // cout << yellow << string(bar_length, '-') << white << "| ";
-
-    // Add a marker if the current bin has strong reponse
-    if (current_bin > max_bin / 2) {
-
-      // Calculate the note of this bin by searching for the current bin
-      // frequency in the notes map. But use the note *preceding* the insertion
-      // point returned by lower bound. Also nudge the bin frequency a
-      // microtone, otherwise exact frequencies will be mapped to the previous
-      // note.
-      const auto note = riff::notes.lower_bound(bin_freq);
-      cout << red << bin_freq << white << " " << note->second << endl;
-    }
-  }
+  // Dump the list of keys
+  cout << "----" << endl;
+  for (const auto &i : key_list)
+    cout << i.first << endl;
 
 } catch (const std::exception &e) {
   std::cout << "Caught " << e.what() << std::endl;
