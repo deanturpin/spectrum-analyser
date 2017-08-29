b58aa2d4e6dcdab9085a91aac7213b2d58d28866
deanturpin <git@lvbtn.uk>
2017-08-29 13:01:36 +0100

    Simple window peak detect

--- tempo.cpp
+++ tempo.cpp
@@ -1,6 +1,5 @@
 #include "notes.h"
 #include "riff.h"
-// #include "fourier.h"
 #include <algorithm>
 #include <iostream>
 
@@ -9,19 +8,46 @@ void tempo() try {
 
   using namespace std;
 
-  const auto samples = rif::read_samples(8000);
 
   cout << "TEMPO" << endl;
+  cout << "Collecting ... " << endl;
+
+  const unsigned int batch_size = 4000;
+  auto samples = rif::read_samples(batch_size);
   cout << "Sample rate " << rif::header.sample_rate << " Hz" << endl;
   cout << "Samples " << samples.size() << endl;
 
-  // for (const auto &i : samples)
-    // cout << i << endl;
-
   const auto minmax = minmax_element(samples.cbegin(), samples.cend());
 
-  cout << *minmax.first << endl;
-  cout << *minmax.second << endl;
+  map<unsigned int, short> histogram;
+
+  const unsigned int bins = 100;
+  const unsigned int scale = batch_size  / bins;
+  for (unsigned int i = 0; i < batch_size; ++i)
+    ++histogram[i / scale] += samples.at(i);
+
+  // for (const auto &i : histogram)
+    // cout << i.first << "\t" << i.second << endl;
+
+  // Peak detector
+  const unsigned int window = 1;
+  const auto threshold = *minmax.second / 5;
+
+  for (unsigned int i = window; i < histogram.size() - window; ++i) {
+
+    const auto previous = histogram.at(i - window);
+    const auto current = histogram.at(i);
+    const auto next = histogram.at(i + window);
+
+    cout << current << "\t";
+    cout << previous << "\t";
+    cout << next << "\t";
+
+    if (current > 0 && (current - previous) > threshold && (current - next) > threshold)
+      cout << "*";
+
+    cout << endl;
+  }
 
 } catch (const std::exception &e) {
   std::cout << "Caught " << e.what() << std::endl;
